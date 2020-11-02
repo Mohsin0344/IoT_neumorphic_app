cd5167b5140d6bade0bd1d451c2ecbd420ee5cb4
Flutter <mk19981997@gmail.com>
2020-11-03 00:37:57 +0500

    Initial commit

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0XFF26282B);
+Color activeColor1 = Color(0XFFCD2A12);
+Color activeColor2 = Color(0XFFED560D);
+
+List<Color> gradientColors = [activeColor1,activeColor2];
